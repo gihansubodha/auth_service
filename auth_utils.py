82f0ab5ea850b2a3347114d79d98fe1748166636
Gihan Subodha <gihansubodha@gmail.com>
2025-08-01 22:10:41 +0530

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,22 +1,32 @@
 import jwt
-import datetime
+from datetime import datetime, timedelta
+from flask import request, jsonify
+from functools import wraps
 
-SECRET_KEY = "cozy_secret"
+SECRET_KEY = "my_secret_key"
 
-def generate_token(user):
+def generate_token(user_id, role):
     payload = {
-        'id': user['id'],
-        'username': user['username'],
-        'role': user['role'],
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=9999)
+        'user_id': user_id,
+        'role': role,
+        'exp': datetime.utcnow() + timedelta(hours=5)
     }
     return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-def verify_token(token):
-    try:
-        decoded = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        return decoded
-    except jwt.ExpiredSignatureError:
-        return None
-    except jwt.InvalidTokenError:
-        return None
+def verify_token():
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = None
+            if 'Authorization' in request.headers:
+                token = request.headers['Authorization'].split(" ")[1]
+            if not token:
+                return jsonify({'message': 'Token is missing!'}), 403
+            try:
+                data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+                request.user = data
+            except:
+                return jsonify({'message': 'Token is invalid!'}), 403
+            return f(*args, **kwargs)
+        return decorated
+    return decorator
