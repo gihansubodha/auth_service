f12be42a9332c4a42fee583f7f6e527b58fe0216
Gihan Subodha <gihansubodha@gmail.com>
2025-08-01 18:44:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from db_config import get_connection
+from auth_utils import generate_token, verify_token
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.json
+    username = data["username"]
+    password = generate_password_hash(data["password"])
+    role = data["role"]
+
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    try:
+        cursor.execute("INSERT INTO users (username, password, role) VALUES (%s, %s, %s)", (username, password, role))
+        conn.commit()
+        return jsonify({"message": "User registered."})
+    except:
+        return jsonify({"error": "Username already exists."}), 400
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data["username"]
+    password = data["password"]
+
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users WHERE username=%s", (username,))
+    user = cursor.fetchone()
+
+    if user and check_password_hash(user["password"], password):
+        token = generate_token(user)
+        return jsonify({"token": token, "role": user["role"]})
+    return jsonify({"error": "Invalid credentials"}), 401
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    token = request.headers.get("Authorization").split(" ")[1]
+    decoded = verify_token(token)
+    if decoded:
+        return jsonify(decoded)
+    return jsonify({"error": "Invalid token"}), 401
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db_config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="cozycomfort-gihansubodha-soc.c.aivencloud.com",
+	port=26728,
+        user="avnadmin",
+        password="AVNS_i33CBpI3jeyig2mnoMR",
+        database="defaultdb"
+    )
