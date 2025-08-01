5c5697b81a35ea503c965699eeb3b6b13d9125fd
Gihan Subodha <gihansubodha@gmail.com>
2025-08-01 22:25:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,9 @@ def login():
         return jsonify({"token": token, "role": user['role']})
     return jsonify({"message": "Invalid credentials"}), 401
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
